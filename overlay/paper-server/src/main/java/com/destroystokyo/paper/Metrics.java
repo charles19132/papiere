84a0110eb941adf6478434154df69c33d5f43cd2
Charles <c4a@monocles.eu>
2025-07-29 13:26:50 -0400

    Update Metrics.java

--- overlay/paper-server/src/main/java/com/destroystokyo/paper/Metrics.java
+++ overlay/paper-server/src/main/java/com/destroystokyo/paper/Metrics.java
@@ -3,7 +3,7 @@ package com.destroystokyo.paper;
 public class Metrics {
   public static class PaperMetrics {
     public static void startMetrics() {
-      return;
+      return; // Papiere - Remove telemetry
     }
   }
 }
